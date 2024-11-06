d956c17b8b69b2b712ca83ae2d14b25ad4e9b53f
gabriel10987 <gabrielccama66@gmail.com>
2024-11-06 16:16:39 -0500

    Initial Commit

---
+++ JSONRESTful/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONRESTful
+//
+//  Created by Gabriel Anderson Ccama Apaza on 6/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
